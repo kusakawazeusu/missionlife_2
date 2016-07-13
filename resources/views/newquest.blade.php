44e96434140b5784e5f5ba970179630f2783750b
kusakawa <kusakawazeusu@gmail.com>
2016-07-13 15:51:35 +0800

    0714

--- resources/views/newquest.blade.php
+++ resources/views/newquest.blade.php
@@ -34,8 +34,15 @@
             	            <div class="form-group">
              	               	<label for="description" class="col-md-2 col-md-offset-2 control-label">任務描述</label>
              	               	<div class="col-md-6">
-             	               		<textarea id="description" class="form-control" name="description"></textarea>
+             	               		<textarea id="description" rows="5" class="form-control" name="description"></textarea>
              	           		</div>
+             	          	</div>
+            	            <div class="form-group">
+             	               	<label for="salary" class="col-md-2 col-md-offset-2 control-label">給予薪資</label>
+             	               	<div class="col-md-2">
+             	               		<input id="salary" class="form-control" name="salary" placeholder="120">
+             	           		</div>
+             	           		<label for="salary" class="control-label">元</label>
              	          	</div>
                         	<div class="form-group">
                         	    <div class="col-md-6 col-md-offset-4">
